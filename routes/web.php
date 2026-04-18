2c195e01c80bd76efb70c41116241ab02d8e1b42
ArielJambul <arielputralfisyah@gmail.com>
2026-04-18 17:26:49 +0700

    [TBPSKE-36] feat: Pembuatan Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JournalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route Resource untuk Jurnal Refleksi Mandiri
+// Middleware auth digunakan langsung di Controller (atau bisa juga ditambahkan di sini, tapi karena controller Anda sudah menggunakan $this->middleware('auth'), ini cukup)
+Route::resource('journals', JournalController::class);
