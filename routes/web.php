88a2896fd5a9cdd483d659001f7c9e1af48964b6
Wqrpath <salmanalfarisy2005@gmail.com>
2026-04-19 15:38:37 +0700

    TBPSKE-37TBPSKE-37

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AdminController;
+
+// Grouping route admin agar tidak konflik
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::get('/rekrutmen', [AdminController::class, 'rekrutmen'])->name('rekrutmen');
+    Route::get('/laporan', [AdminController::class, 'laporan'])->name('laporan');
+    Route::delete('/forum/{id}/delete', [AdminController::class, 'hapusPostingan'])->name('forum.delete');
+    Route::get('/spesialisasi', [AdminController::class, 'spesialisasi'])->name('spesialisasi');
+});
