96ede51e40f65498276ad09cd884a53de193c5a8
Yuhaa-keren <naufalyuha29@gmail.com>
2026-04-30 02:17:54 +0700

    TBPSKE-2
    error fixed

--- resources/views/journals/layout.blade.php
+++ resources/views/journals/layout.blade.php
@@ -38,7 +38,7 @@
         <div class="flex-grow">
             <h2 class="text-[16px] font-extrabold text-[#111827] px-8 mb-4">Menu</h2>
             <nav class="flex flex-col space-y-1">
-                <a href="{{ url('/') }}" class="sidebar-link">
+                <a href="{{ route('homepage') }}" class="sidebar-link">
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                     Homepage
                 </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         <div class="px-4 py-4 flex-1 overflow-y-auto">
             <h3 class="px-4 text-sm font-semibold text-gray-900 mb-4">Menu</h3>
             <nav class="space-y-2">
-                <a href="#" class="flex items-center gap-4 px-4 py-3 text-gray-700 font-semibold rounded-lg hover:bg-gray-50 transition">
+                <a href="{{ route('homepage') }}" class="flex items-center gap-4 px-4 py-3 text-gray-700 font-semibold rounded-lg hover:bg-gray-50 transition">
                     <svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
                     <span>Homepage</span>
                 </a>
